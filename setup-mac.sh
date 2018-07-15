798879834adc2ab4ba75407f730f627aa8497d65
Roberto Selbach <r@rst.sh>
2018-07-15 13:50:36 -0400

    Add minikube and docker

--- setup-mac.sh
+++ setup-mac.sh
@@ -9,6 +9,7 @@ brew install git
 brew install vim
 brew install go
 brew install wget
+brew install minikube
 
 brew cask install 1password
 brew cask install google-chrome
@@ -19,7 +20,7 @@ brew cask install rambox
 brew cask install slack
 brew cask install vmware-fusion
 brew cask install google-cloud-sdk
-
+brew cask install docker
 # restore my vim settings
 git clone --recursive git@github.com:rselbach/.vim.git ~/.vim
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
