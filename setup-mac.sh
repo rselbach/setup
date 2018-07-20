df632bbc88210dd63849415bef3c27973c47eaa0
Roberto Selbach <rselbach@moncoyote.com>
2018-07-20 06:47:18 -0400

    Use a separate file for bashrc stuff

--- setup-mac.sh
+++ setup-mac.sh
@@ -29,29 +29,4 @@ ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 git config --global user.name "Roberto Selbach"
 git config --global user.email "r@rst.sh"
 
-cat >> $HOME/.bash_profile <<EOF
-SSH_ENV=\$HOME/.ssh/environment
-
-function start_agent {
-     echo "Initialising new SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > \${SSH_ENV}
-     echo succeeded
-     chmod 600 \${SSH_ENV}
-     . \${SSH_ENV} > /dev/null
-     /usr/bin/ssh-add;
-}
-
-# Source SSH settings, if applicable
-
-if [ -f "\${SSH_ENV}" ]; then
-     . \${SSH_ENV} > /dev/null
-     ps -efp \${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-         start_agent;
-     }
-else
-     start_agent;
-fi
-
-export GOPATH=\$HOME/go
-export PATH=\$PATH:\$GOPATH/bin
-EOF
+cat bashrc >> $HOME/.bash_profile
