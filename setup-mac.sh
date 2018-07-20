7b91f592bfe8389806c06f962539f106ad20bd0b
Roberto Selbach <rselbach@moncoyote.com>
2018-07-20 06:54:22 -0400

    Config vscode

--- setup-mac.sh
+++ setup-mac.sh
@@ -13,7 +13,6 @@ brew install minikube
 
 brew cask install 1password
 brew cask install google-chrome
-brew cask install visual-studio-code
 brew cask install spotify
 brew cask install iterm2
 brew cask install rambox
@@ -21,6 +20,7 @@ brew cask install slack
 brew cask install vmware-fusion
 brew cask install google-cloud-sdk
 brew cask install docker
+
 # restore my vim settings
 git clone --recursive git@github.com:rselbach/.vim.git ~/.vim
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
@@ -29,4 +29,12 @@ ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 git config --global user.name "Roberto Selbach"
 git config --global user.email "r@rst.sh"
 
+
+# install and set up vscode
+
+brew cask install visual-studio-code
+. ./vscode.sh
+
+
+# add stuff to bash_profile
 cat bashrc >> $HOME/.bash_profile
---
+++ vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension bungcip.better-toml
+code --install-extension eamodio.gitlens
+code --install-extension ms-python.python
+code --install-extension ms-vscode.Go
+code --install-extension shuworks.vscode-table-formatter
+code --install-extension stkb.rewrap
+code --install-extension zxh404.vscode-proto3
