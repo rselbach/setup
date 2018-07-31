45d41093f49c65b9a3d248bf35e1f792eec07859
Roberto Selbach <r@rst.sh>
2018-07-31 14:08:23 -0400

    Don't prompt on install

--- setup-debian.sh
+++ setup-debian.sh
@@ -15,7 +15,7 @@ aptinstall build-essential
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install spotify-client
+aptistall spotify-client
 
 # restore my vim settings
 git clone --recursive git@github.com:rselbach/.vim.git ~/.vim
@@ -35,14 +35,14 @@ sudo apt-get -f install
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk google-cloud-sdk-app-engine-go
+sudo apt-get update && aptinstall google-cloud-sdk google-cloud-sdk-app-engine-go
 
 
 # install vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update && sudo apt-get install -y code
+sudo apt-get update && aptinstall -y code
 
 . ./vscode.sh
 
