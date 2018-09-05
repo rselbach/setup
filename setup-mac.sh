1607c4e17fc24238cca407bab4ad0eea2a02b16d
Roberto Selbach <rselbach@moncoyote.com>
2018-09-05 10:54:57 -0400

    reorg

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,13 +5,39 @@ if [[ ! -f "/usr/local/bin/brew" ]]; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+#########################
+# install and config zsh
+#########################
 brew install zsh
+ln -sf zshrc $HOME/.zshrc
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+
+#############
+# set up git
+#############
 brew install git
+git config --global user.name "Roberto Selbach"
+git config --global user.email "r@rst.sh"
+
+
 brew install vim
+git clone --recursive git@github.com:robteix/.vim.git ~/.vim
+ln -sf $HOME/.vim/vimrc $HOME/.vimrc
+
+############################
+# install and set up vscode
+############################
+brew cask install visual-studio-code
+. ./vscode.sh
+
+
+# install other tools
+#
 brew install go
 brew install wget
 brew install minikube
-
 brew cask install 1password
 brew cask install google-chrome
 brew cask install spotify
@@ -21,21 +47,4 @@ brew cask install vmware-fusion
 brew cask install google-cloud-sdk
 brew cask install docker
 
-# restore my vim settings
-git clone --recursive git@github.com:robteix/.vim.git ~/.vim
-ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 
-# setup git
-git config --global user.name "Roberto Selbach"
-git config --global user.email "r@rst.sh"
-
-
-# install and set up vscode
-
-brew cask install visual-studio-code
-. ./vscode.sh
-
-# config zsh
-cp -f zshrc $HOME/.zshrc
-
-git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
