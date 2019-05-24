548f2a4c9e718725668054974db67d37fc12c7e0
Roberto Selbach <r@rst.sh>
2019-05-24 06:25:24 -0700

    Modularize more installs

--- setup-debian.sh
+++ setup-debian.sh
@@ -11,60 +11,16 @@ aptinstall() {
 aptinstall vim git wget curl
 aptinstall build-essential
 
-# spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-aptinstall spotify-client
-
-# restore my vim settings
-git clone --recursive git@github.com:robteix/.vim.git ~/.vim
-
-git config --global user.name "Roberto Selbach"
-git config --global user.email "r@rst.sh"
-
-
-aptinstall libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-sudo apt-get -f install
-
-
-# install vscode
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update && aptinstall -y code
-
-. ./vscode.sh
-
-
 # direnv
-sudo apt-get install direnv
+aptinstall direnv
 
 # can't live without jump
 JUMPVER=0.22.0
 wget https://github.com/gsamokovarov/jump/releases/download/v${JUMPVER}/jump_${JUMPVER}_amd64.deb
 sudo dpkg -i jump_${JUMPVER}_amd64.deb
 
-
-
-# config zsh
-cp -f zshrc $HOME/.zshrc
-git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-
 # ctags with go support
 ln -snf $(pwd)/ctags $HOME/.ctags
-
-# fix keyboard
-sed -e 's,\xc4\x86,\xc3\x87,g' \
-    -e 's,\xc4\x87,\xc3\xa7,g' \
-    < /usr/share/X11/locale/en_US.UTF-8/Compose > $HOME/.XCompose
-
-sudo su -c 'cat >> /etc/environment <<EOF
-GTK_IM_MODULES=cedilla
-QT_IM_MODULES=cedilla
-EOF'
 
 do_source() {
 	local fn="$1"
--- setup-mac.sh
+++ setup-mac.sh
@@ -5,23 +5,11 @@ if [[ ! -f "/usr/local/bin/brew" ]]; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-#############
-# set up git
-#############
+####################
+# install essentials
+####################
 brew install git
-git config --global user.name "Roberto Selbach"
-git config --global user.email "r@rst.sh"
-
-
 brew install vim
-git clone --recursive git@github.com:robteix/.vim.git ~/.vim
-ln -sf $HOME/.vim/vimrc $HOME/.vimrc
-
-############################
-# install and set up vscode
-############################
-brew cask install visual-studio-code
-. ./vscode.sh
 
 
 # exuberant ctags with go support
@@ -30,7 +18,6 @@ ln -snf ctags $HOME/.ctags
 
 # install other tools
 #
-brew install go
 brew install wget
 brew install minikube
 brew cask install 1password
@@ -39,7 +26,6 @@ brew cask install spotify
 brew cask install iterm2
 brew cask install slack
 brew cask install vmware-fusion
-brew cask install google-cloud-sdk
 brew cask install docker
 brew install jump
 brew install direnv
