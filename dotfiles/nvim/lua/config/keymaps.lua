12e232a9c58d42c5b7418e978609a20c9aea5dc2
Roberto Selbach <rselbach@rselbach.com>
2025-10-12 16:20:58 -0400

    fix keymap

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -3,3 +3,5 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+vim.api.nvim_set_keymap("i", "<C-_>", "<C-o>", { noremap = true })
