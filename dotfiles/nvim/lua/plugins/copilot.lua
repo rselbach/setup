970008dd0f0576bfb7f9ba43b138d6a94498323b
Roberto Selbach <rselbach@ibm.com>
2025-10-14 12:45:28 -0400

    zsh

--- dotfiles/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -17,4 +17,10 @@ return {
       help = true,
     },
   },
+  config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+        vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', { silent = true, expr = true })
+        vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Dismiss()', { silent = true, expr = true })
+    end,
 }
