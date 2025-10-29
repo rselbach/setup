46b78a60f0f684ff2604da43261d7664e6e8678b
Roberto Selbach <rselbach@rselbach.com>
2025-10-29 18:50:34 -0400

    Organize Go imports on save

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,70 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
+      setup = {
+        gopls = function(_, opts)
+          -- Organize imports on save
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+              local clients = vim.lsp.get_clients({ bufnr = 0, name = "gopls" })
+              if #clients == 0 then return end
+              local offset_encoding = clients[1].offset_encoding or "utf-16"
+              local params = vim.lsp.util.make_range_params(nil, offset_encoding)
+              params.context = { only = { "source.organizeImports" } }
+              local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+              for cid, res in pairs(result or {}) do
+                for _, r in pairs(res.result or {}) do
+                  if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                  end
+                end
+              end
+            end,
+          })
+        end,
+      },
     },
   },
 }
